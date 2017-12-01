0efd2a9c3d1d8645ef3ce7d4629ebdc320f84ebc
richardNing <richard.ning@gmail.com>
2017-12-01 14:12:10 +0800

    v1.0

---
+++ src/main/java/cn/omg/action/UserAction.java
@@ -0,0 +1,11 @@
+package cn.omg.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction  extends ActionSupport{
+    public String execute() throws Exception {
+//        System.out.print("---------------userAction---------------");
+        System.out.print("----33-----------userAction\"---------------");
+        return  NONE;
+    }
+}
